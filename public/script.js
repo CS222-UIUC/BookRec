357b8e4880e654899beacda9102ee879b8dcec1c
Davidlasky <davidjiangxc@gmail.com>
2022-11-25 02:23:23 -0600

    added google login

---
+++ public/script.js
@@ -0,0 +1,30 @@
+function readlistone() {
+  const listone = document.querySelector(".booklistone");
+  const booklistone = document.querySelector("#listoneupload").files[0];
+  const reader = new FileReader();
+  reader.addEventListener(
+    "load",
+    () => {
+      // this will then display a text file
+      listone.innerText = reader.result;
+    },
+    false
+  );
+  reader.onerror = (err) => console.log(err);
+  reader.readAsText(booklistone);
+}
+
+function readlisttwo() {
+  const listtwo = document.querySelector(".booklisttwo");
+  const booklisttwo = document.querySelector("#listtwoupload").files[0];
+  const reader = new FileReader();
+  reader.addEventListener(
+    "load",
+    () => {
+      listtwo.innerText = reader.result;
+    },
+    false
+  );
+  reader.onerror = (err) => console.log(err);
+  reader.readAsText(booklisttwo);
+}
